f09e9840be11d0775157d662855fd94d156402d4
Sara Salah <93276124+SaraSallah@users.noreply.github.com>
2023-07-11 05:58:59 +0300

    add design system

---
+++ app/src/main/java/com/example/donuts_app/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.donuts_app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Background = Color(0xFFFED8DF)
+val PrimaryColor = Color(0xFFFF7074)
+val SecondaryColor = Color(0xFFD7E4F6)
+val White = Color(0xFFFFFFFF)
+val PinkBackground = Color(0xFFFFC7D0)
+val Black = Color(0xFF000000)
+val Black80 = Color(0xCC000000)
+val Black60 = Color(0x99000000)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
